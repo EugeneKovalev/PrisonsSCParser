a90cca267ef151f120c55d1efbf2599ae446c91d
Евгений Ковалёв <disaharid@gmail.com>
2016-11-21 14:38:29 +0300

    first commit

---
+++ helpers.py
@@ -0,0 +1,51 @@
+def write_name(scs_file, **kwargs):
+    scs_file.write(kwargs.get('system_name') + ' => nrel_main_idtf:' + '\n')
+    scs_file.write('   [' + kwargs.get('name') + '](* <- lang_ru;; *);;' + '\n')
+
+
+def write_zip_code(scs_file, **kwargs):
+    scs_file.write(kwargs.get('system_name') + ' => nrel_zip_code:' + '\n')
+    scs_file.write('   [' + kwargs.get('zip_code') + '](* <- lang_ru;; *);;' + '\n')
+
+
+def write_address(scs_file, **kwargs):
+    scs_file.write(kwargs.get('system_name') + ' => nrel_address:' + '\n')
+    scs_file.write('   [' + kwargs.get('address') + '](* <- lang_ru;; *);;' + '\n')
+
+
+def write_image(scs_file, **kwargs):
+    scs_file.write(kwargs.get('system_name') + ' <- rrel_key_sc_element:' + '\n')
+    scs_file.write('    ' + kwargs.get('system_name') + '_Image (*' + '\n')
+    scs_file.write('    ' + '=> nrel_main_idtf:' + '\n')
+    scs_file.write('    ' + ' [' + kwargs.get('name') + ' - Изображение](* <- lang_ru;; *);;' + '\n')
+    scs_file.write('    ' + '<-sc_illustration;;' + '\n')
+    scs_file.write('    ' + '<= nrel_sc_text_translation: ...' + '\n')
+    scs_file.write(2 * '    ' + '(*' + '\n')
+    scs_file.write(2 * '    ' + '->rrel_example:' + '"file://' + kwargs.get('image_name') + '"(* <-image;; *);;' + '\n')
+    scs_file.write(2 * '    ' + '*);;' + '\n')
+    scs_file.write('*);;' + '\n')
+
+
+def write_geolocation(scs_file, **kwargs):
+    scs_file.write(kwargs.get('system_name') + ' => nrel_geographical_location: ...' + '\n')
+    scs_file.write('(* <-concept_mapped_point;; \n')
+    scs_file.write('=> nrel_WGS_84_translation: [{0}, {1}] \n'.format(kwargs.get('lat'), kwargs.get('lng')))
+    scs_file.write('(* -> ... (* < - concept_coordinate;; \n')
+    scs_file.write('->rrel_latitude: ... (* <= nrel_value: ... \n')
+    scs_file.write('(* -> rrel_degree: {0};; * );; * );; \n'.format(kwargs.get('lat')))
+    scs_file.write('->rrel_longitude: ... (* <= nrel_value: ... \n')
+    scs_file.write('(* -> rrel_degree: {0};; \n'.format(kwargs.get('lng')))
+    scs_file.write('* );;* );;* );;* );;* );; \n')
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import json
+import googlemaps
+import random
+import AdvancedHTMLParser
+from urllib.request import Request, urlopen, urlretrieve
+from urllib.parse import quote
+
+from helpers import write_name, write_zip_code, write_address, write_image, write_geolocation
+
+parser = AdvancedHTMLParser.AdvancedHTMLParser()
+parser.parseStr(urlopen("http://zoneby.net/catalog/iu_by/").read().decode("windows-1251", "replace"))
+plain_prisons_text = parser.getElementsByClassName('doctext').getElementsByTagName('p')
+prisons_by_regions = [x for x in plain_prisons_text[2:14] if not x.attributes]
+
+parsed_prisons = []
+for prisons_block in prisons_by_regions:
+    prisons = prisons_block.innerHTML.split('<p')[0].split('&nbsp;<br />')
+    for prison in prisons:
+        try:
+            prison_components = [x.strip() for x in prison.split('<br />') if x.strip()]
+            name = prison_components[0]
+            zip_code = prison_components[1].split(',')[0].strip()
+            address = ','.join(prison_components[1].split(',')[1:])
+            phones = []
+            for phone_info in prison_components[2:]:
+                phones.extend(phone_info.split('тел.')[-1].split(','))
+
+            parsed_prisons.append({
+                'name': name,
+                'address': address,
+                'zip_code': zip_code,
+                'phones': phones
+            })
+        except IndexError:
+            continue
+
+gmaps = googlemaps.Client(key='AIzaSyAs5bmk9_Vs586Q8s2ExTVwz7_rcmzl7xY')
+
+for prison in parsed_prisons[0:5]:
+    scs_file = open(prison['name'] + '.scs', 'w')
+    system_name = 'prison_' + str(random.getrandbits(30))
+    write_name(scs_file, **{'system_name': system_name, 'name': prison['name']})
+    write_zip_code(scs_file, **{'system_name': system_name, 'zip_code': prison['zip_code']})
+    write_address(scs_file, **{'system_name': system_name, 'address': prison['address']})
+
+    geocode_result = gmaps.geocode(prison['address'])
+    if geocode_result:
+        write_geolocation(scs_file, **{'system_name': system_name,
+                                       'lat': geocode_result[0]['geometry']['location']['lat'],
+                                       'lng': geocode_result[0]['geometry']['location']['lng']})
+
+    request_url = 'http://35.161.57.139:8080/gpix/v1/gpix?keyword={0}&limit=1'.format(quote(prison['name']))
+    request = Request(request_url)
+    request.add_header('Authorization', 'QsiP2ZX7Ps')
+    response = json.loads(urlopen(request).read().decode('utf-8'))
+    image_url = response['data']['images'][0]['image_url']
+    image_name = "{0}.{1}".format(prison['name'], image_url.split('.')[-1])
+    urlretrieve(image_url, image_name)
+    write_image(scs_file, **{'system_name': system_name, 'name': prison['name'], 'image_name': image_name})
